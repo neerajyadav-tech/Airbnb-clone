5e9338c2712f1b629c5ee92735bd56ff7a8c2d82
Neeraj Yadav <ny3623652@gmail.com>
2026-04-18 11:21:59 +0530

    first commit

---
+++ backend/config/db.js
@@ -0,0 +1,22 @@
+// import mongoose from "mongoose";
+
+// const connectDb = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGODB_URL)
+//         console.log("DB connected")
+//     } catch (error) {
+//         console.log("db error")
+//     }
+// }
+import mongoose from "mongoose";
+
+const connectDb = async () => {
+  try {
+    await mongoose.connect(process.env.MONGODB_URL);
+    console.log("✅ DB Connected");
+  } catch (err) {
+    console.log("❌ DB Error:", err.message);
+  }
+};
+
+export default connectDb;
